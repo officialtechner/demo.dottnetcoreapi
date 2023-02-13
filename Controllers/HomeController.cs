06e7fba3f9d1e5eee6d54aad597a015f6cdab985
Nitin <chdnitinsharma@gmail.com>
2023-02-13 17:35:56 +0530

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace test_demo_api.Controllers
+{
+    [ApiController]
+    [Route("test/[controller]")]
+    public class HomeController : ControllerBase
+    {
+
+        [Route("phone1")]
+        [HttpGet]
+        public string Index()
+        {
+            return "tsssss - Home - Index";
+        }
+
+
+        [Route("phone")]
+        [HttpGet]
+        public string Phone()
+        {
+            return "tsssss - Home - Phone";
+        }
+
+
+        [Route("[action]")]
+        [HttpGet]
+        public string Country()
+        {
+            return "tsssss - Home - Country";
+        }
+
+        [Route("")]
+        [HttpGet]
+        public string Test()
+        {
+            return "tsssss - Home - Test";
+        }
+
+    }
+}
